b13383230830f8eea65c2432bf19df0f932e5467
namitha1711 <namithar.24.beai@acharya.ac.in>
2025-05-24 14:15:18 +0530

    Create  ConwaysGameOfLife.py

---
+++ ConwaysGameOfLife.py
@@ -0,0 +1,15 @@
+
+def comma_code(lst):
+    if len(lst) == 0:
+        return ''
+    elif len(lst) == 1:
+        return lst[0]
+    else:
+        return ', '.join(lst[:-1]) + ', and ' + lst[-1]
+
+def main():
+    spam = ['apples', 'bananas', 'tofu', 'cats']
+    print(comma_code(spam))
+
+if __name__ == "__main__":
+    main()
